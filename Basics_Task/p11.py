aaad9a23a705b1a47a42308f925c9a1fe49a91ba
Rifat Miah <rifatmiah2041@gmail.com>
2026-06-21 23:28:43 +0600

    Added basic tasks p11

---
+++ Basics_Task/p11.py
@@ -0,0 +1,9 @@
+# Import a module of your choice and show some operations. (User input)
+
+import math
+num = float(input("Enter a number: "))
+
+print("\nSquare Root    = ", math.sqrt(num))
+print("Power (number)^2 = ", math.pow(num, 2))
+print("Ceiling value    = ", math.ceil(num))
+print("Floor value      =", math.floor(num))
