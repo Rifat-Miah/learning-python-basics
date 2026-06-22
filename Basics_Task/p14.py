b459562a9b2739db252b4f15625b98e0653728d2
Rifat Miah <rifatmiah2041@gmail.com>
2026-06-22 23:13:31 +0600

    Added basic tasks p14

---
+++ Basics_Task/p14.py
@@ -0,0 +1,12 @@
+# Create a list of 10 numbers and find sum, max, min.
+
+numbers = [67, 34, 30, 20, 55, 95, 84, 23, 16, 33]
+
+add = sum(numbers)
+maximum = max(numbers)
+minimum = min(numbers)
+
+print("List of the numbers = ", numbers)
+print("\nSum is = ", add)
+print("Maximum value = ", maximum)
+print("Minimum value = ", minimum)
