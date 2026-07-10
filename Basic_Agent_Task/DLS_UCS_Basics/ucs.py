bb903ed7a276a8809cc70f0e1a9ebd28825b4e53
Rifat Miah <rifatmiah2041@gmail.com>
2026-07-10 23:44:20 +0600

    Uniform Cost Search (UCS) on a graph task added.

--- Basic_Agent_Task/DLS_UCS_Basics/ucs.py
+++ Basic_Agent_Task/DLS_UCS_Basics/ucs.py
@@ -7,3 +7,38 @@ graph = {
 'D': [('B', 5), ('C', 1)] 
 }
 '''
+
+graph = { 
+    'A': [('B', 1), ('C', 4)], 
+    'B': [('A', 1), ('C', 2), ('D', 5)], 
+    'C': [('A', 4), ('B', 2), ('D', 1)], 
+    'D': [('B', 5), ('C', 1)] 
+}
+
+def ucs(start, goal):
+
+    frontier = [(start, 0, [start])] # (Node, Cost, Path)
+
+    explored = []
+
+    while len(frontier) > 0:
+        frontier.sort(key=lambda x: x[1])
+
+        node, cost, path = frontier.pop(0)
+
+        print("Visit:", node, "Cost:", cost)
+
+        if node == goal:
+            print("\nGoal Found.")
+            print("Path:", " --> ".join(path))
+            print("Total Cost: ", cost)
+            return
+        explored.append(node)
+
+        for child, weight in graph[node]:
+            if child not in explored:
+                frontier.append((child, cost + weight, path + [child]))
+
+    print("Goal Not Found.")
+
+ucs('A', 'D')
