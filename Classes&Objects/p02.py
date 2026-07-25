9662f1e87959eeb78d6844cb8c0ebebd017f310c
Rifat Miah <rifatmiah2041@gmail.com>
2026-07-25 20:56:40 +0600

    Classes & object task p02 added

---
+++ Classes&Objects/p02.py
@@ -0,0 +1,14 @@
+class student:
+    def __init__(self, name, roll, marks):
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+
+    def average(self):
+        return sum(self.marks)/len(self.marks)
+
+student1 = student("Rifat", 33, [78, 87, 92, 85, 75.90])
+print("Student Name: ", student1.name)
+print("Student Roll: ", student1.roll)
+print("Sudent marks: ", student1.marks)
+print("Student average mark = ", student1.average())
